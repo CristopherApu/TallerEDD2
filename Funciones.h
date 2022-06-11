05f3c305dc33c3a7e42e844fb1b943c730aba3be
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2022-06-11 02:05:07 -0400

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -1,11 +1,21 @@
-#ifndef MARKS_FUNCIONES_H
-#define MARKS_FUNCIONES_H
+#ifndef TALLER2EDD_FUNCIONES_H
+#define TALLER2EDD_FUNCIONES_H
 
     void saludo();
     void ConsultaNombreBar();
-    void ConsultaCantidadProductos();
     void AgregaNombreProductos();
     void AgregaPrecioProductos();
     void despedida();
+
+/*struct Listas{
+
+    char NombreBar[80];
+    int QuiereAgregarOtroProducto;
+    char NombreProducto;
+    int PrecioProducto;
+    char NombreBarModificado[90];
+    char ruta[] = ("./Listas_Disponibles/");
+
+};*/
 
 #endif
