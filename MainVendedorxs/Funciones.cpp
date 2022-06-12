f5d1277879b25c0abecbd742d8b63c80fcd85896
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2022-06-12 17:44:48 -0400

    Update Funciones.cpp

--- MainVendedorxs/Funciones.cpp
+++ MainVendedorxs/Funciones.cpp
@@ -1,5 +1,8 @@
 #include "Funciones.h"
 #include <cstdio>
+//#include <iostream>
+//#include <fstream>
+//#include <cstring>
 
 void saludo(){
 
@@ -15,19 +18,28 @@ void ConsultaNombreBar(){
 
 void AgregaNombreProductos(){
 
+    FILE * arch2 = fopen(Lista.NombreBarModificado, "a+");
     printf("Cual es el nombre del producto?\n");
+    scanf("%s", Productos.NombreProducto);
+    fprintf(arch2, "%s\n", Productos.NombreProducto);
+    fclose(arch2);
 
 }
 
 void AgregaPrecioProductos(){
 
+    FILE * arch2 = fopen(Lista.NombreBarModificado, "a+");
     printf("Cual es el precio del producto?\n");
+    scanf("%d", Productos.PrecioProducto);
+    fprintf(arch2, "%d\n", Productos.PrecioProducto);
+    fclose(arch2);
 
 }
 
 void ConsultaSiQuiereAgregarOtroProducto(){
 
     printf("Quiere agregar otro producto?\n[1] Si\n[2] No\n");
+    scanf("%d", Productos.QuiereAgregarOtroProducto);
 
 }
 
