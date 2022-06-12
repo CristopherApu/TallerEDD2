f10c201e4174f097a62deab10c165dace22c886a
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2022-06-12 18:39:44 -0400

    Update Funciones.cpp

--- MainVendedorxs/Funciones.cpp
+++ MainVendedorxs/Funciones.cpp
@@ -4,21 +4,16 @@
 //#include <fstream>
 //#include <cstring>
 
-void saludo(){
-
+void Saludo(){
     printf("Hola, bienvenidx\n");
-
 }
 
 void ConsultaNombreBar(){
-
     printf("Como se llama el local?         (hasta 80 caracteres)\n");
     scanf("%s", Lista.NombreBar);
-
 }
 
-void modificador(){
-
+void Modificador(){
     FILE * arch = fopen("modificador.txt", "w");
     fprintf(arch, "Lista_");
     fprintf(arch, "%s", Lista.NombreBar);
@@ -26,38 +21,29 @@ void modificador(){
     fclose(arch);
     arch = fopen("modificador.txt", "r");
     fscanf(arch, "%s", Lista.NombreBarModificado);
-
 }
 
 void AgregaNombreProductos(){
-
     FILE * arch2 = fopen(Lista.NombreBarModificado, "a+");
     printf("Cual es el nombre del producto?\n");
     scanf("%s", Productos.NombreProducto);
     fprintf(arch2, "%s\n", Productos.NombreProducto);
     fclose(arch2);
-
 }
 
 void AgregaPrecioProductos(){
-
     FILE * arch2 = fopen(Lista.NombreBarModificado, "a+");
     printf("Cual es el precio del producto?\n");
     scanf("%d", Productos.PrecioProducto);
     fprintf(arch2, "%d\n", Productos.PrecioProducto);
     fclose(arch2);
-
 }
 
 void ConsultaSiQuiereAgregarOtroProducto(){
-
     printf("Quiere agregar otro producto?\n[1] Si\n[2] No\n");
     scanf("%d", Productos.QuiereAgregarOtroProducto);
-
 }
 
-void despedida(){
-
+void Despedida(){
     printf("Lista creada!");
-
 }
