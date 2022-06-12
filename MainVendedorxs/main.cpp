6e657cbb8e4168ea98d999c73eafd05b8b6d04bb
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2022-06-12 17:45:27 -0400

    Update main.cpp

--- MainVendedorxs/main.cpp
+++ MainVendedorxs/main.cpp
@@ -7,48 +7,32 @@ using namespace std;
 
 int main(){
 
-    char NombreBar[80];
-    int QuiereAgregarOtroProducto;
-    char NombreProducto;
-    int PrecioProducto;
-    char NombreBarModificado[90];
-    //char ruta[] = "./Listas_Disponibles/";
-
     FILE * arch = fopen("modificador.txt", "w");
 
     saludo();
 
     ConsultaNombreBar();
-    scanf("%s", NombreBar);
+    scanf("%s", Lista.NombreBar);
 
     fprintf(arch, "Lista_");
-    fprintf(arch, "%s", NombreBar);
+    fprintf(arch, "%s", Lista.NombreBar);
     fprintf(arch, ".txt\n");
     fclose(arch);
 
     arch = fopen("modificador.txt", "r");
-    fscanf(arch, "%s", NombreBarModificado);
+    fscanf(arch, "%s", Lista.NombreBarModificado);
 
-    //printf(strcat(ruta, NombreBarModificado));
-
-    FILE * arch2 = fopen(/*strcat(ruta, */NombreBarModificado/*)*/, "a+");
+    FILE * arch2 = fopen(Lista.NombreBarModificado, "a+");
 
     printf("Ingrese el primer producto:\n");
 
     do {
 
         AgregaNombreProductos();
-        scanf("%s", NombreProducto);
-        fprintf(arch2, "%s\n", NombreProducto);
-        
         AgregaPrecioProductos();
-        scanf("%d", PrecioProducto);
-        fprintf(arch2, "%d\n", PrecioProducto);
-
         ConsultaSiQuiereAgregarOtroProducto();
-        scanf("%d", QuiereAgregarOtroProducto);
 
-    } while (QuiereAgregarOtroProducto == 1);
+    } while (Productos.QuiereAgregarOtroProducto == 1);
 
     fclose(arch2);
 
